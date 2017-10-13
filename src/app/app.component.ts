74528887d481dea4fca727b09e85fab47fd0a959
daya apraj <dayaapraj123@gmail.com>
2017-10-13 21:14:38 +0530

    first commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
-
+import {Http,Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'daya pooja';
   dob = '1221'
+  name =["pooja","daya","tanu"]
+  //private myUrl = 'https://address-book-demo.herokuapp.com/api/contacts';
+   private myUrl = 'http://192.168.0.104/icittest/sql_php.php';
+  data: any = {};
+   HEROES = [
+    {id: 1, name:'Superman'},
+    {id: 2, name:'Batman'},
+    {id: 5, name:'BatGirl'},
+    {id: 3, name:'Robin'},
+    {id: 4, name:'Flash'}
+];
+
+constructor(private http:Http){
+	this.getContacts();
+	
+	
 }
+getData(){
+	return this.http.get(this.myUrl)
+	.map((res:Response) => res.json())
+	
+}
+getContacts(){
+	this.getData().subscribe(data => {
+		this.data =data
+		console.log(data)
+	})
+	
+}
+}
+	
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+//import { LoginComponent } from './login/login.component';
+//import { AlertService, AuthenticationService } from './../service/index';
+//import { ServiceComponent } from './service/service.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
+ 
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	HttpModule
+	
   ],
   providers: [],
   bootstrap: [AppComponent]
